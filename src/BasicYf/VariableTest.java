6f8952c1549752e1ae9e0c438b6df3e9863ccc9c
chenzhengwen <747298086@qq.com>
2021-02-07 15:08:57 +0800

    基本语法学习

---
+++ src/BasicYf/VariableTest.java
@@ -0,0 +1,13 @@
+package BasicYf;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = num1 + num2;
+        System.out.println("num3= "+num3);
+        int num4 = 50;
+        int num5 = num4 - num3;
+        System.out.println("num5= "+num5);
+    }
+}
